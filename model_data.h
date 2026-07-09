1d2b303900e6d8aca20d9420014eebc06ab9d705
NitishDwi07 <152952688+NitishDwi07@users.noreply.github.com>
2026-07-09 12:57:07 +0530

    Add on-device ML glue (wombcare_ml) + firmware fix/integration docs

---
+++ model_data.h
@@ -0,0 +1,6 @@
+#ifndef WOMBCARE_MODEL_DATA_H
+#define WOMBCARE_MODEL_DATA_H
+#include <stdint.h>
+extern const unsigned char g_wombcare_nsp_model[];
+extern const unsigned int g_wombcare_nsp_model_len;
+#endif
